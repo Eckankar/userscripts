aab5e460c2614b1c11a9c033e2aef66bf5183342
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2014-08-22 14:51:54 +0200

    10 bids != 0 bids

--- Miscellaneous/ebay.highlight-bids.user.js
+++ Miscellaneous/ebay.highlight-bids.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://mathemaniac.org
 // @include        http://*.ebay.*/*
 // @grant      none
-// @version    2.3.0
+// @version    2.3.1
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @description    Hilights items that have bids with a red border and yellow background.
 // ==/UserScript==
@@ -14,7 +14,7 @@
 $('document').ready(function() {
     $(".bids span").each(function() {
         // Skip listings with no bids.
-        if ($(this).text().match(/0 bids/)) return;
+        if ($(this).text().match(/\b0 bids/)) return;
 
         $(this).closest('li[listingid]').css({
             "border": "3px solid red",
