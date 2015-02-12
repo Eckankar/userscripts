a205caefe68d4c9279aaa86470c30974da226874
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2015-02-12 21:56:18 +0100

    Update eBay highlight script to work with new classes

--- Miscellaneous/ebay.highlight-bids.user.js
+++ Miscellaneous/ebay.highlight-bids.user.js
@@ -3,18 +3,18 @@
 // @namespace      http://mathemaniac.org
 // @include        http://*.ebay.*/*
 // @grant      none
-// @version    2.3.1
+// @version    2.3.2
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 // @description    Hilights items that have bids with a red border and yellow background.
 // ==/UserScript==
 
-// Based on http://userscripts-mirror.org/scripts/show/66089.html v.2.2.1
+// Based on http://userscripts.org/users/126140 v.2.2.1
 // Updated for newer eBay layout.
 
 $('document').ready(function() {
-    $(".bids span").each(function() {
+    $(".lvprices .lvformat").each(function() {
         // Skip listings with no bids.
-        if ($(this).text().match(/\b0 bids/)) return;
+        if ($(this).text().match(/\b0 bids/) || !$(this).text().match(/\d+ bids/)) return;
 
         $(this).closest('li[listingid]').css({
             "border": "3px solid red",
