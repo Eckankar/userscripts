efab08b989e84c1bba4ab710420e1d21e19be15e
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2016-06-06 11:09:36 +0200

    Update version number

--- YouTube/youtube.hide-recommendedforyou.user.js
+++ YouTube/youtube.hide-recommendedforyou.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        YouTube Hide "Recommended for you" from related videos
 // @namespace   http://mathemaniac.org/
-// @version     1.0b
+// @version     1.0.1
 // @description Hides videos marked as "Recommended for you" from the related videos on YouTube video pages.
 // @include     http://youtube.com/watch*
 // @include     http://www.youtube.com/watch*
 // @include     https://youtube.com/watch*
 // @include     https://www.youtube.com/watch*
 // @grant       none
-// @copyright   2013, Sebastian Paaske Tørholm
+// @copyright   2013-2016, Sebastian Paaske Tørholm
 // @require     http://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
