563468a5d8fb2efd74c19d09b6e78cecbf2a97a4
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2016-12-09 22:44:58 +0100

    Create gaia-ad-remover.user.js

---
+++ Gaia/gaia-ad-remover.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Gaia Ad Remover
+// @namespace  http://mathemaniac.org/
+// @version    1.0.0
+// @description  Removes ads from Gaia Online
+// @match      http://www.gaiaonline.com/*
+// @copyright  2015-2016, Sebastian Paaske Tørholm
+// @require    https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
+// @grant none
+// ==/UserScript==
+
+$('.bannerads, .gaia-ad').remove();
