d66e27d5bb8aaaab6fbad6c6ffd0bfa80da38a9e
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2017-08-02 20:52:31 +0200

    Update LinkedIn PYMK script to work with new page
    Addresses https://greasyfork.org/en/forum/discussion/27748/missing-url

--- LinkedIn/linkedin.pymk-email-remover.user.js
+++ LinkedIn/linkedin.pymk-email-remover.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name       LinkedIn hide email contacts from "People You May Know"
 // @namespace  http://mathemaniac.org
-// @version    1.1.0
+// @version    1.2.0
 // @description  Hides those annoying email contacts from PYMK
-// @match      http://www.linkedin.com/people/pymk*
-// @match      https://www.linkedin.com/people/pymk*
-// @copyright  2015, Sebastian Paaske Tørholm
+// @match      https://www.linkedin.com/mynetwork*
+// @copyright  2015-2017, Sebastian Paaske Tørholm
 // @require    https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
 // ==/UserScript==
 
 $(function () {
-    $('ul.people-cards-list li[id*="@"]').remove();
+    $('.mn-pymk-list__card:has(.mn-person-info__guest-handle)').remove();
 
     // Remove when autoloading new ones
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
@@ -19,14 +18,14 @@ $(function () {
         mutations.forEach( function (mutation) {
             if (mutation.addedNodes) {
                 $(mutation.addedNodes).each( function () {
-                    if ($(this).attr('id').match(/card-.*@.*/)) {
-                        console.log("removing ", $(this).attr('id'));
-                        $(this).remove();
+                    if ($(this).hasClass('mn-person-info__guest-handle')) {
+                        console.log("removing card for ", $(this).text());
+                        $(this).parents('.mn-pymk-list__card').remove();
                     }
                 } );
             }
         });
     });
 
-    $('ul.people-cards-list').each( function () { observer.observe(this, config); } );
+    $('.mn-pymk-list__cards').each( function () { observer.observe(this, config); } );
 });
