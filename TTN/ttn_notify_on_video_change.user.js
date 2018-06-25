d4ab5e157a6228e93810fcd184d46fd604a37c04
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2018-06-25 21:01:03 +0200

    TTN: Notify on video change

---
+++ TTN/ttn_notify_on_video_change.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name       TTN notify on change
+// @namespace  http://mathemaniac.org/
+// @version    1.0.0
+// @description  Notifies when the content changes on TTN.
+// @match        https://www.treesnetwork.com/*
+// @copyright  2018, Sebastian Paaske Tørholm
+// @require    https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
+// @grant none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+$(function () {
+    var last_name = undefined;
+    function checkForChange() {
+        var title = $('div span span[class*="name"]').text();
+        if (! last_name) last_name = title;
+
+        if (title !== last_name) {
+            last_name = title;
+            if (document.hidden && title !== "Off The Air") {
+                var n = new Notification("Now playing on TTN: " + title, { "tag": "ttn_now_playing", "requireInteraction": true, "silent": false } );
+            }
+        }
+    }
+    setInterval(checkForChange, 1000);
+});
