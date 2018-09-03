3c10791c9e6599746cb3115a21cff7b9508f4415
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2018-09-03 20:47:26 +0200

    Newer version!

--- TTN/batch_add_videos_on_ttn.user.js
+++ TTN/batch_add_videos_on_ttn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Batch-add videos on Trees Network
 // @namespace  http://mathemaniac.org/
-// @version    1.0.0
+// @version    1.1.0
 // @description  Allows batch-adding videos on Trees Network
 // @match        https://www.treesnetwork.com/*
 // @copyright  2018, Sebastian Paaske Tørholm
@@ -14,27 +14,48 @@
 (function () {
     var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
 
+    var title;
     var videosToAdd;
 
     var addSingleVideo;
     var addSingleVideoPt2;
 
     addSingleVideo = function () {
-        var url = videosToAdd.shift();
-        if (! url) return;
+        var vid = videosToAdd.shift();
+        if (! vid) return;
+
+        title = "";
+        var url;
+        if (typeof vid === 'object') {
+            url = vid.url;
+            title = vid.title;
+        }
 
         var $field = $('input[placeholder="To add a video, paste a supported URL here..."]');
         var $btn = $field.next();
         nativeInputValueSetter.call($field[0], url);
 
-        var ev2 = new Event('input', { bubbles: true});
+        var ev2 = new Event('input', { bubbles: true });
         $field[0].dispatchEvent(ev2);
 
         $btn.click();
-        setTimeout(addSingleVideoPt2, 2000);
+        setTimeout(addSingleVideoPt2, 5000);
     };
 
     addSingleVideoPt2 = function () {
+        if (title) {
+            var $field = $('input[placeholder="Type a name for the video..."]');
+
+            nativeInputValueSetter.call($field[0], title);
+
+            var ev2 = new Event('input', { bubbles: true });
+            $field[0].dispatchEvent(ev2);
+        }
+
+        setTimeout(addSingleVideoPt3, 2 * 60 * 1000);
+    };
+
+    addSingleVideoPt3 = function () {
         var submit = $('button:contains("Submit Video")');
         submit.click();
 
@@ -44,6 +65,19 @@
     window.batchAddVideos = function(videos) {
         videosToAdd = videos;
         addSingleVideo();
+    };
+
+    window.parseAndBatchAddVideos = function (showTitle, episodes) {
+        var videos = [];
+        var eps = episodes.split(/\n/);
+        for (var i = 0; i < eps.length; i++) {
+            var m = eps[i].match(/^(https?\S+)(?:[\s-]+)(.*)$/);
+            if (!m) continue;
+
+            videos.push( { url: m[1], title: showTitle + " - " + m[2] } );
+        }
+
+        batchAddVideos(videos);
     };
 
 })();
