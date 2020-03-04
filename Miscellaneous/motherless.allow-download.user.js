18b6426a82d68aaa566e1da184849bd1b69daec9
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2020-03-04 09:08:14 +0100

    Script to download from Motherless

---
+++ Miscellaneous/motherless.allow-download.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name       Allow download on Motherless
+// @namespace  http://mathemaniac.org/
+// @version    1.0.0
+// @description  Yes; it really is that simple.
+// @match        https://motherless.com/*
+// @copyright  2020, Sebastian Paaske Tørholm
+// @grant none
+// ==/UserScript==
+/* jshint -W097 */
+/* eslint-env jquery */
+'use strict';
+
+__is_premium = true;
