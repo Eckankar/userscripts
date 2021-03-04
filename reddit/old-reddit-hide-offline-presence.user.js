c357c7f4b726ef1b97c3b368dab45789df4734b1
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2021-03-04 11:34:58 +0100

    Make script run on document start to help on dot flashes

--- reddit/old-reddit-hide-offline-presence.user.js
+++ reddit/old-reddit-hide-offline-presence.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Hide offline presence dot on old reddit
 // @namespace    https://mathemaniac.org
-// @version      1.0
+// @version      1.0.1
 // @description  Hides the offline presence dot from old reddit. Only hides offline presence, so you notice if you're displayed as online. https://old.reddit.com/r/changelog/comments/lx08r2/announcing_online_presence_indicators/
 // @author       Sebastian Paaske Tørholm
 // @match        https://old.reddit.com/*
 // @icon         https://www.google.com/s2/favicons?domain=reddit.com
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
