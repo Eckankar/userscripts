cdc2dc87cf041a47fbe467040a71f6d5874239a2
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2021-03-05 15:49:45 +0100

    Handle document.head not being ready yet.

--- reddit/old-reddit-hide-offline-presence.user.js
+++ reddit/old-reddit-hide-offline-presence.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide offline presence dot on old reddit
 // @namespace    https://mathemaniac.org
-// @version      1.0.1
+// @version      1.0.2
 // @description  Hides the offline presence dot from old reddit. Only hides offline presence, so you notice if you're displayed as online. https://old.reddit.com/r/changelog/comments/lx08r2/announcing_online_presence_indicators/
 // @author       Sebastian Paaske Tørholm
 // @match        https://old.reddit.com/*
@@ -13,7 +13,16 @@
 (function() {
     'use strict';
 
-    document.head.insertAdjacentHTML('beforeend', `
+    function removeDot() {
+        if (! document.head) {
+            setTimeout(removeDot, 50);
+            return;
+        }
+
+        document.head.insertAdjacentHTML('beforeend', `
 <style>#header-bottom-right .presence_circle.offline { display: none !important; }</style>
-    `);
+        `);
+    }
+
+    removeDot();
 })();
