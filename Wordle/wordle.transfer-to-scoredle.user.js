49189f0d9be2e8e0bc7712742148803fe79501b4
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2024-01-05 00:08:18 +0100

    Add script to transfer result from Wordle to Scoredle

---
+++ Wordle/wordle.transfer-to-scoredle.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Transfer Wordle to Scoredle
+// @namespace    https://mathemaniac.org/
+// @version      1.0.0
+// @description  Adds a button to the Wordle completion screen, to transfer today's game into Scoredle in a new tab.
+// @match        https://www.nytimes.com/games/wordle/index.html
+// @match        https://scoredle.com/*
+// @copyright    2024, Sebastian Paaske Tørholm
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+if (document.location.host === 'www.nytimes.com') {
+    // Wordle
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var config = { childList: true, characterData: false, attributes: false, subtree: true };
+    var observer = new MutationObserver( function (mutations) {
+        mutations.forEach( function (mutation) {
+            for (const node of mutation.addedNodes) {
+                if (node.nodeType === Node.TEXT_NODE) continue;
+
+                let shareButton = node.querySelector('[class^="Footer-module_shareButton"]');
+                if (shareButton) {
+                    let newButton = shareButton.cloneNode(true);
+                    newButton.querySelector('svg').remove();
+                    newButton.querySelector('span').innerHTML = "Open in Scoredle";
+                    newButton.setAttribute('style', 'margin-top: 0.25em;');
+                    newButton.setAttribute('id', 'export-to-scoredle-btn');
+                    newButton.onclick = async function () {
+                        let userid = 1*document.cookie.match(/(?<=nyt-jkidd=uid=)\d+/)[0];
+                        if (! userid) { userid = 'ANON'; }
+
+                        let data = JSON.parse( localStorage.getItem(`nyt-wordle-moogle/${userid}`) );
+
+                        let guesses = data.game.boardState.filter((e) => e);
+
+                        let solution;
+                        if (data.game.status === 'WIN') {
+                            solution = guesses[guesses.length-1];
+                        } else {
+                            const todayDate = new Date();
+                            const dateString = todayDate.toISOString().split('T')[0];
+                            const response = await fetch(`https://www.nytimes.com/svc/wordle/v2/${dateString}.json`)
+                            const result = await response.json();
+
+                            solution = result.solution;
+                        }
+
+                        guesses = guesses.join(",");
+
+                        window.open(`https://scoredle.com/#guesses=${guesses};solution=${solution}`, '_blank');
+                    };
+                    shareButton.after(newButton);
+                }
+            }
+        });
+    });
+
+    observer.observe(document.querySelector('body'), config);
+} else {
+    // Scoredle
+
+    let matches = document.location.hash.match(/^#guesses=([^;]+);solution=(.*)$/);
+    if (! matches) return;
+
+    let guesses = matches[1].split(',');
+    let solution = matches[2];
+
+    function inputValue(id, value) {
+        let answerBox = document.querySelector(`#${id}`);
+        answerBox.value = value;
+        answerBox._valueTracker.setValue('');
+        answerBox.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+
+    inputValue('answer', solution);
+    for (var i = 0; i < guesses.length; i++) {
+        inputValue(`guess${i}`, guesses[i]);
+    }
+}
