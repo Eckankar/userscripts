be7570bbb938c5af5c03eee67c694c5365cd4501
Sebastian Paaske Tørholm <Eckankar@gmail.com>
2025-07-30 14:41:52 +0200

    Update the eBay bid highlighter
    * Works again
    * Doesn't use jQuery anymore.
    * Also works in German now as requested by https://greasyfork.org/en/scripts/4578-ebay-hilight-items-with-bids/discussions/235155

--- Miscellaneous/ebay.highlight-bids.user.js
+++ Miscellaneous/ebay.highlight-bids.user.js
@@ -1,25 +1,23 @@
 // ==UserScript==
 // @name           eBay - Hilight Items With Bids
-// @namespace      http://mathemaniac.org
+// @namespace      https://mathemaniac.org
 // @include        http://*.ebay.*/*
 // @include        https://*.ebay.*/*
 // @grant      none
-// @version    2.3.4
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
+// @version    2.4.0
 // @description    Hilights items that have bids with a red border and yellow background.
 // ==/UserScript==
 
 // Based on http://userscripts.org/users/126140 v.2.2.1
 // Updated for newer eBay layout.
 
-$(function() {
-    $(".lvprices .lvformat").each(function() {
-        // Skip listings with no bids.
-        if ($(this).text().match(/\b0 bids/) || !$(this).text().match(/\d+ bids?/)) return;
+document.querySelectorAll('.srp-results .s-item').forEach((item) => {
+    const bidBox = item.querySelector('.s-item__bidCount');
+    if (! bidBox) return;
 
-        $(this).closest('li[listingid]').css({
-            "border": "3px solid red",
-            "background-color": "yellow"
-        });
-    });
+    const bidText = bidBox.innerText;
+    if (bidText.match(/^\s*\b0\b/) || ! bidText.match(/^\s*\d+/)) return;
+
+     item.style.border = "3px solid red";
+     item.style.backgroundColor = "yellow";
 });
